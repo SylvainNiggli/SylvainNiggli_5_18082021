527952edcb5153b5c493a620d81d99a67deeefeb
sylvain.niggli <sylvain.niggli@gmail.com>
2021-09-28 17:57:20 +0200

    amélioration validation formulaire

--- js/command.js
+++ js/command.js
@@ -1,51 +1,57 @@
 //Fonction d'envoie de la requête de commande
 function sendCommand(e) {
-    let contact = {
-        firstName: document.getElementById("firstName").value,
-        lastName: document.getElementById("lastName").value,
-        address: document.getElementById("address").value,
-        city: document.getElementById("city").value,
-        email: document.getElementById("email").value
-    };
+  const contact = {
+    firstName: document.getElementById("firstName").value,
+    lastName: document.getElementById("lastName").value,
+    address: document.getElementById("address").value,
+    city: document.getElementById("city").value,
+    email: document.getElementById("email").value
+  };
+  let products = JSON.parse(localStorage.getItem("cart"));
+  let productList = [];
 
-    let products = JSON.parse(localStorage.getItem("cart"));
-    let productList = [];
+  for(element of products){
+    productList.push(element.id);
+  }
 
-    for(element of products){
+  let data = {
+      contact: contact,
+      products: productList
+  };
 
-      productList.push(element.id);
-    }
-
-    let data = {
-        contact: contact,
-        products: productList
-    };
-
-    e.preventDefault();
-    const options = {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        'Accept': 'application/json', 
-        'Content-Type': 'application/json'
+  e.preventDefault();
+  const options = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      'Accept': 'application/json', 
+      'Content-Type': 'application/json'
       }     
-    };
-    fetch("http://localhost:3000/api/cameras/order", options)
-    .then(function(res) {
-      if (res.ok) {
-        return res.json();
-      }
-    })
-    .then(function(value) {
-          console.log(value);
-          localStorage.setItem("confirmation", JSON.stringify(value));
-          window.location = "confirmation.html";
-    });
+  };
+  fetch("http://localhost:3000/api/cameras/order", options)
+  .then(function(res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function(value) {
+    console.log(value);
+    localStorage.setItem("confirmation", JSON.stringify(value));
+    window.location = "confirmation.html";
+  });
 }
 
 //Mise sur écoute de l'évènement "submit" avec appel de la fonction de commande
 //lorsque l'évènement se produit
-document
-  .getElementById("form")
-  .addEventListener("submit", sendCommand);
+let form = document.getElementById("form");
+
+form.addEventListener("submit", function(event) {
+    if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+    } else{
+        sendCommand(event);
+    }
+    form.classList.add('was-validated')
+  });
   
